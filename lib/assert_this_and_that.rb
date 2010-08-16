3028abce4023a2a412c0460b4be091cc41d78202
Jake <github@jake.otherinbox.com>
2010-08-16 10:16:12 -0700

    Still deving

---
+++ lib/assert_this_and_that.rb
@@ -0,0 +1,10 @@
+#
+#	This may require the addition of other gem requirements
+#
+require 'assert_this_and_that/accessible_via_format'
+require 'assert_this_and_that/accessible_via_protocol'
+require 'assert_this_and_that/accessible_via_user'
+require 'assert_this_and_that/should_act_as_list'
+require 'assert_this_and_that/should_have_association'
+require 'assert_this_and_that/should_require_association'
+require 'assert_this_and_that/should_require_attribute'
