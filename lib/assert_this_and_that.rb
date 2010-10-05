060a07a469ffb67d03c56334f9fcb989c828a6d2
Jake <github@jake.otherinbox.com>
2010-10-05 09:45:26 -0700

    Missed a spot.

--- lib/assert_this_and_that.rb
+++ lib/assert_this_and_that.rb
@@ -7,8 +7,8 @@ end
 #	This may require the addition of other gem requirements
 #
 require 'assert_this_and_that/declarative'
-require 'assert_this_and_that/should_act_as_list'
-require 'assert_this_and_that/should_have_association'
-require 'assert_this_and_that/should_require_association'
+require 'assert_this_and_that/assertions'
+require 'assert_this_and_that/acts_as_list'
+require 'assert_this_and_that/associations'
 require 'assert_this_and_that/attributes'
 require 'assert_this_and_that/action_controller_extension'
