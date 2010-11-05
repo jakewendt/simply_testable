ebfbf258cabf8eafd6e343024e5c7e50fe5183c6
Jake <github@jake.otherinbox.com>
2010-11-05 12:26:15 -0700

    Building generate to create/append .autotest and Rakefile.

---
+++ generators/simply_testable/simply_testable_generator.rb
@@ -0,0 +1,88 @@
+class SimplyTestableGenerator < Rails::Generator::Base
+
+	def manifest
+		#	See Rails::Generator::Commands::Create
+		#	rails-2.3.10/lib/rails_generator/commands.rb
+		#	for code methods for record (Manifest)
+		record do |m|
+#
+#	Create/append a generic .autotest that check config/autotests/
+#		(with unique marker)
+#
+#	Create/append Rakefile to check config/rakefiles/
+#
+
+
+#	
+#				File.open('Rakefile','a'){|f| 
+#					f.puts <<-EOF
+#	#	From `script/generate simply_pages` ...
+#	require 'simply_pages/test_tasks'
+#					EOF
+#				}
+#	
+#				File.open('.autotest','a'){|f| 
+#					f.puts <<-EOF
+#	#	From `script/generate simply_pages` ...
+#	require 'simply_pages/autotest'
+#					EOF
+#				}
+#	
+#				%w( create_pages ).each do |migration|
+#					m.migration_template "migrations/#{migration}.rb",
+#						'db/migrate', :migration_file_name => migration
+#				end
+#	
+#				m.directory('public/javascripts')
+#				Dir["#{File.dirname(__FILE__)}/templates/javascripts/*js"].each{|file| 
+#					f = file.split('/').slice(-2,2).join('/')
+#					m.file(f, "public/javascripts/#{File.basename(file)}")
+#				}
+#				m.directory('public/stylesheets')
+#				Dir["#{File.dirname(__FILE__)}/templates/stylesheets/*css"].each{|file| 
+#					f = file.split('/').slice(-2,2).join('/')
+#					m.file(f, "public/stylesheets/#{File.basename(file)}")
+#				}
+#	#			m.directory('test/functional/pages')
+#	#			Dir["#{File.dirname(__FILE__)}/templates/functional/*rb"].each{|file| 
+#	#				f = file.split('/').slice(-2,2).join('/')
+#	#				m.file(f, "test/functional/pages/#{File.basename(file)}")
+#	#			}
+#	#			m.directory('test/unit/pages')
+#	#			Dir["#{File.dirname(__FILE__)}/templates/unit/*rb"].each{|file| 
+#	#				f = file.split('/').slice(-2,2).join('/')
+#	#				m.file(f, "test/unit/pages/#{File.basename(file)}")
+#	#			}
+		end
+	end
+
+end
+module Rails::Generator::Commands
+	class Create
+		def migration_template(relative_source, 
+				relative_destination, template_options = {})
+			migration_directory relative_destination
+			migration_file_name = template_options[
+				:migration_file_name] || file_name
+			if migration_exists?(migration_file_name)
+				puts "Another migration is already named #{migration_file_name}: #{existing_migrations(migration_file_name).first}: Skipping" 
+			else
+				template(relative_source, "#{relative_destination}/#{next_migration_string}_#{migration_file_name}.rb", template_options)
+			end
+		end
+	end #	Create
+	class Base
+	protected
+		#	the loop through migrations happens so fast
+		#	that they all have the same timestamp which
+		#	won't work when you actually try to migrate.
+		#	All the timestamps MUST be unique.
+		def next_migration_string(padding = 3)
+			@s = (!@s.nil?)? @s.to_i + 1 : if ActiveRecord::Base.timestamped_migrations
+				Time.now.utc.strftime("%Y%m%d%H%M%S")
+			else
+				"%.#{padding}d" % next_migration_number
+			end
+		end
+	end	#	Base
+end
